3a8d4fad57f63f03680e8c9fbabf9a1741f8793b
Alex S <88047550+SaxleS@users.noreply.github.com>
2023-08-16 16:00:53 +0300

    coomit 2.1

--- js/curentDate.js
+++ js/curentDate.js
@@ -2,9 +2,11 @@
 window.addEventListener('load', 
  function (e) {
   var d = new Date();
-  var day = d.getDate(); if (day<10) day='0'+day;
+  var day = d.getDate();
+    let day2 = day+8;
+  if (day<10) day='0'+day;
   var month = d.getMonth() + 1; if (month < 10 ) month='0'+month;
   var year = d.getFullYear(); 
   document.getElementById("mydate").value = year + "-" + month + "-" + day;
-  document.getElementById("mydate2").value = year + "-" + month + "-" + day;
+  document.getElementById("mydate2").value = year + "-" + month + "-" + day2;
  }, false);
